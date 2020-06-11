d46a8d36cf8280c0ba8c43483806fe67bcbcee6f
aj-fuwa <65690123+aj-fuwa@users.noreply.github.com>
2020-06-11 18:11:00 +0200

    Update fermat_factorization.py

--- fermat_factorization.py
+++ fermat_factorization.py
@@ -4,20 +4,23 @@
 # Die Sprache des Programms: Python
 '''
 import math as math
+import sympy as sp
 ########################################################################################################################
 # Step 1: Input the number n
 n = int(input("Input the number to be factorised (Fermat's Method): "))
+
+if (sp.isprime(n)): # if the number is prime, then exit
+    print("Number entered:", n, "is prime. No factors!")
+    exit()
 ########################################################################################################################
 # Step 2: Find x and y
 index = 1
 x = 0
-flag = False    # will be set to True if number entered is not prime
-                # else it will be False, if the number entered is prime
+
 while(True):
     y = math.sqrt(n + math.pow(index, 2))
 
     if (y.is_integer()):    # type of y is float technically but the number is checked
-        flag = True         # set to True since factors found
         x = index
         break
 
@@ -28,4 +31,6 @@ print("Values of x and y are: x=", x," y=", y)
 # Step 3: Find the factors f1 and f2
 f1 = math.fabs(x - y)
 f2 = math.fabs(x + y)
+
 print("Factors are: ", f1, "and ", f2)
+
